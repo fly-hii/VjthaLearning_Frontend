4d20b4cb2aee05cda808df0e542300db5c12cb12
Praveen598 <jeennalapraveen@gmail.com>
2025-06-25 14:52:46 +0530

    Refactor: Remove unused UserIcon import in AdminLayout component

--- src/components/admin/AdminLayout.tsx
+++ src/components/admin/AdminLayout.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { UserIcon } from '@heroicons/react/24/solid';
+
 import { 
   LayoutDashboard, 
   FileText, 
